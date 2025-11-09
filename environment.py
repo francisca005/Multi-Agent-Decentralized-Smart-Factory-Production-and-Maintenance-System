c2f7a8dc95f26d12d39b6092e238cf03a84d1615
Francisca Cerqueira <162129943+francisca005@users.noreply.github.com>
2025-11-09 13:23:43 +0000

    Refactor Environment class to FactoryEnvironment

--- environment.py
+++ environment.py
@@ -1,10 +1,8 @@
 # environment.py
-class Environment:
+class FactoryEnvironment:
     def __init__(self):
         self.time = 0
-        self.events = []
         self.metrics = {
-            # fase 2
             "requests_ok": 0,
             "requests_refused": 0,
             "delivered_flour": 0,
@@ -14,17 +12,10 @@ class Environment:
             "repairs_started": 0,
             "repairs_finished": 0,
             "machine_downtime_ticks": 0,
-            # fase 3 (CNP)
+            # --- CNP metrics ---
             "cnp_cfp": 0,
-            "cnp_proposals": 0,
             "cnp_accepts": 0,
-            "cnp_rejects": 0,
-            "cnp_wins_supplier_a": 0,
-            "cnp_wins_supplier_b": 0,
         }
 
     def tick(self):
         self.time += 1
-
-env = Environment()
-
